198ca5b592081426e54dddc6691e2efe0c286c2a
Poco <xuzhiming1997@163.com>
2022-08-01 10:22:52 +0800

    add

---
+++ .history/手写代码/Javascript/手写JSON/json_parse_20220730145046.js
@@ -0,0 +1,9 @@
+
+
+
+JSON.parse('{}');              // {}
+JSON.parse('true');            // true
+JSON.parse('"foo"');           // "foo"
+JSON.parse('[1, 5, "false"]'); // [1, 5, "false"]
+JSON.parse('null');            // null
+JSON.parse('1');               //  1
